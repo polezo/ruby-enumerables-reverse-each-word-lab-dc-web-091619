93c6900a3c5daccd5d1e469724f9da24ac738f30
Adam Finch <atyreefinch@gmail.com>
2019-08-07 14:07:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word (string)
   arr = string.split(" ")
-  arr2 = arr.each { |word| word.reverse! }
+  arr2 = arr.collect { |word| word.reverse! }
   reversed_string = arr2.join(" ")
 end
